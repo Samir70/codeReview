6f38830b16c19bf7a30c45bf9b8741a9a64fa5ab
Samir70 <sbetmouni@talktalk.net>
2020-11-25 21:34:18 +0000

    enable choosing of questions

--- qList.js
+++ qList.js
@@ -2,4 +2,4 @@ import {sortColors1pass, sortColors2pass} from './codeToReview/sortColors.js';
 
 export const qList = [
     sortColors1pass, sortColors2pass
-]
+].map((x, i) => {return {...x, id:i}})
