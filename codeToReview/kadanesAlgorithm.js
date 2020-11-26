d65b5280d17e918e4f59cf8db11bb2f1ce752cf2
Samir70 <sbetmouni@talktalk.net>
2020-11-26 07:07:44 +0000

    add kadane's algorithm for maxSubarraySum

---
+++ codeToReview/kadanesAlgorithm.js
@@ -0,0 +1,20 @@
+export const kadane = {
+    name: "Kadane's algorithm to find maximum subarray sum",
+    code: [
+        "const kadane = (arr) => {",
+        "    if (arr.length === 1) {return arr[0]}",
+        "    var maxToHere = arr[0], maxSoFar = arr[0];",
+        "    for (var i = 1; i<arr.length; i++) {",
+        "        if (maxToHere + arr[i] > arr[i]) {",
+        "            // get better subArray ending here by including ith element",
+        "            maxToHere += arr[i];",
+        "        } else {",
+        "            maxToHere = arr[i];",
+        "        }",
+        "        if (maxToHere > maxSoFar) { maxSoFar = maxToHere }",
+        "        // console.log('maxToHere, maxSoFar', maxToHere, maxSoFar)",
+        "    }",
+        "    return maxSoFar",
+        "}"
+    ]
+};
--- qList.js
+++ qList.js
@@ -1,5 +1,7 @@
 import {sortColors1pass, sortColors2pass} from './codeToReview/sortColors.js';
+import {kadane} from './codeToReview/kadanesAlgorithm.js'
 
 export const qList = [
-    sortColors1pass, sortColors2pass
+    sortColors1pass, sortColors2pass,
+    kadane
 ].map((x, i) => {return {...x, id:i}})
