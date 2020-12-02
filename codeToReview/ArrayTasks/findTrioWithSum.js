2e22a8961f230ebe5c031b04170a96954ef275d1
Samir70 <sbetmouni@talktalk.net>
2020-12-02 06:55:19 +0000

    add findTrioWithSum of 2020

---
+++ codeToReview/ArrayTasks/findTrioWithSum.js
@@ -0,0 +1,29 @@
+export const findTrioWithSum = {
+    name: "Find three elements in array with sum of 2020",
+    author: "Samir70",
+    language: "javaScript",
+    code: [
+        "const findPairSorted = (sorted, target) => {",
+        "    let left = 0, right = sorted.length - 1;",
+        "    while (left < right) {",
+        "        let a = sorted[left], b = sorted[right];",
+        "        if (a+b === target) {return [a, b]}",
+        "        if (a+b < target) {",
+        "            left++",
+        "        } else {",
+        "            right--",
+        "        }",
+        "    }",
+        "    return -1",
+        "}",
+        "const findTriple = arr => {",
+        "    let sorted = arr.map(Number).sort((a, b) => a-b);",
+        "    for (let i=0; i<sorted.length;i++) {",
+        "        let outcome = findPairSorted(sorted.slice(i+1), 2020-sorted[i]);",
+        "        if (outcome !== -1) {",
+        "            return [sorted[i], ...outcome, 'product is', sorted[i]*outcome[0]*outcome[1]]",
+        "        }",
+        "    }",
+        "}"
+    ]
+}
--- qList.js
+++ qList.js
@@ -1,24 +1,25 @@
-import {sortColors1pass, sortColors2pass} from './codeToReview/sortColors.js';
+import { sortColors1pass, sortColors2pass } from './codeToReview/sortColors.js';
 // array tasks
-import {kadane} from './codeToReview/ArrayTasks/kadanesAlgorithm.js';
-import {mergeIntervals} from './codeToReview/ArrayTasks/mergeIntervals.js'
+import { kadane } from './codeToReview/ArrayTasks/kadanesAlgorithm.js';
+import { mergeIntervals } from './codeToReview/ArrayTasks/mergeIntervals.js';
+import { findTrioWithSum } from './codeToReview/ArrayTasks/findTrioWithSum.js';
 // tree functions
-import {inorderIterative, inorderRecursive} from './codeToReview/treeTraversals.js'
-import {helloWorld} from './codeToReview/helloWorld.js';
+import { inorderIterative, inorderRecursive } from './codeToReview/treeTraversals.js'
+import { helloWorld } from './codeToReview/helloWorld.js';
 // design objects
-import {stockSpanner} from './codeToReview/stockSpan.js'
+import { stockSpanner } from './codeToReview/stockSpan.js'
 // Vue 
-import {vueTodoWComponent} from './codeToReview/Vue/todoListWithComponent.js'
+import { vueTodoWComponent } from './codeToReview/Vue/todoListWithComponent.js'
 
 export const qList = [
     helloWorld,
     sortColors1pass, sortColors2pass,
     // array tasks
-    kadane, mergeIntervals,
+    kadane, mergeIntervals, findTrioWithSum,
     // tree functions
     inorderIterative, inorderRecursive,
     // design objects
     stockSpanner,
     // Vue
     vueTodoWComponent
-].map((x, i) => {return {...x, id:i}})
+].map((x, i) => { return { ...x, id: i } })
